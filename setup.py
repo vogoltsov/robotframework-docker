595c564d3ed6c6d7f636f43612e55f2378d085c8
Vitaly Ogoltsov <vitaly.ogoltsov@me.com>
2021-03-15 17:30:57 +0300

    chore(release): Version 1.1.0.dev1.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
 
 setup(
     name='robotframework-docker',
-    version='1.0.1',
+    version='1.1.0.dev1',
     description='A Robot Framework Docker Library',
     long_description=long_description,
     url='https://github.com/vogoltsov/robotframework-docker',
